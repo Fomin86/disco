8f409db3ab8d005ac2fd37e2c75aa6a7ef550932
Fomin86 <91221571+Fomin86@users.noreply.github.com>
2021-09-23 11:45:19 +0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <FolderView name="$" :folders="folders" :files="files" />
+</template>
+
+<script>
+import FolderView from './components/FolderView.vue'
+import data from './data/list';
+
+export default {
+  name: 'App',
+  components: {
+    FolderView
+  },
+  data: () => data
+}
+</script>
+
+<style lang="scss">
+body {
+  background-color: darkslategray;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 1px solid black;
+  border-radius: 30px;
+  text-align: center;
+  max-width: 400px;
+  width: 100%;
+  padding: 30px;
+  box-shadow: 0 0 70px rgba(50, 205, 50, .2);
+  line-height: 40px;
+}
+</style>
---
+++ src/components/FolderView.vue
@@ -0,0 +1,46 @@
+<template>
+  <p class="folder">{{name}}</p>
+  <ul>
+    <template v-for="folder in folders">
+      <li class="folder_item"> 
+        <FolderView :name="folder.name" :folders="folder.folders" :files="folder.files"/>
+      </li>
+    </template>
+  </ul>
+  <ul class="file_list">
+    <template v-for="file in files">
+      <li class="file_item">
+        <FileView :name="file.name"/>
+      </li>
+    </template>
+  </ul>
+</template>
+
+<script>
+import FileView from "./FileView";
+export default {
+  name: 'FolderView',
+  components: {FileView},
+  props: {
+    name: String,
+    folders:Array,
+    files:Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.folder{
+  color: yellow;
+}
+.folder_item{
+  list-style: none;
+  background: url(../image/folder.png) no-repeat 0 1px;
+}
+.file_item{
+  list-style: none;
+  background: url(../image/file.png) no-repeat 0 1px;
+}
+</style>
